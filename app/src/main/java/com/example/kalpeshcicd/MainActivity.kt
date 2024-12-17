9ba90d54416aa2926545939ed9b62a076409d709
KalpeshDz <kalpesh.kumawat@decipherzone.com>
2024-12-17 15:15:21 +0530

    text change

--- app/src/main/java/com/example/kalpeshcicd/MainActivity.kt
+++ app/src/main/java/com/example/kalpeshcicd/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             KalpeshCiCDTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Android Mobile App",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
